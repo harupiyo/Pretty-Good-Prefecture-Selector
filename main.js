55926c98618a6c17eb9c671658b4806eca4b18aa
Aaron Maywood <am7274183@gmail.com>
2021-12-08 19:39:33 +0900

    外側のdif をクリックできるような仕組みは全くだめだった

--- main.js
+++ main.js
@@ -25,7 +25,6 @@ function selectit(string){
     // 都道府県リストのウィジェットを非表示にし、
     prefecture_list.style.display = "none"
     // modal であったことを示す背景の色を戻し、(本格的なmodal の実装とは異なり、ここでは単にbody の背景色を変えているだけである)
-    prefectures.style.display="block"   // TODO
     body.style.background = "white"
     // 選ばれた都道府県を選択結果として格納
     pref.value=string
@@ -33,60 +32,14 @@ function selectit(string){
 }
 
 // <select>--都道府県--</select> をクリックしたら
-prefectures.addEventListener('click', (event) => {
-    prefectures.style.display="none"    // NG ここでselect 自体を消しても、
-                                        // 1. ポップアップしてくるものを防ぐことができない
-                                        // 2. このselect がなくなることにより、後続のフローに悪影響が出る
+//      ↓
+// <select>--都道府県--</select> を囲うdiv をクリックしたら...は<select> の方が手前なので失敗した
+const pref_wrapper = document.querySelector('.pref-wrapper')
+pref_wrapper.addEventListener('click', (event) => {
     // ウィジェットを表示
     prefecture_list.style.display = "block"
     // modal であることを示すため、背景色を暗くする
     body.style.background = "gray"
     // TODO ブラウザーのselect のプルダウンの表示を禁止したい
     event.preventDefault()
-})
-
-// NG focus イベントは関係がなかった
-// https://developer.mozilla.org/ja/docs/Web/API/GlobalEventHandlers/onfocus
-prefectures.addEventListener('focus',(e)=>{
-    console.log('prefecture focus')
-    e.preventDefault()
-})
-// NG change イベントは関係なかった
-prefectures.addEventListener('change',(e)=>{
-    console.log('prefecture change')
-    e.preventDefault()
-})
-// NG input イベントは関係がなかった
-// https://developer.mozilla.org/ja/docs/Web/API/HTMLSelectElement
-prefectures.addEventListener('input',(e)=>{
-    console.log('prefecture input')
-    e.preventDefault()
-})
-
-// NG <option> 要素は関係がないようだ
-const opt = document.querySelector('option')
-opt.style.display="none"                // これでポップアップするものの内容は消えてくれる
-                                        // ただしborder が残ってしまう
-                                        // click 時にボタンを押しっぱなしにするとborder が出続ける
-opt.style.border = "100px solid red"    // NG こいつ自身のボーダーではないようだ
-prefectures.style.border = "10px solid red" // NG <select> のボーダーとも違う
-// NG
-opt.addEventListener('click', (e) => {
-    console.log('option click')
-    e.preventDefault()
-})
-// NG
-opt.addEventListener('focus', (e) => {
-    console.log('option focus')
-    e.preventDefault()
-})
-// NG
-opt.addEventListener('change',(e)=>{
-    console.log('opt change')
-    e.preventDefault()
-})
-// NG
-opt.addEventListener('input',(e)=>{
-    console.log('opt input')
-    e.preventDefault()
 })
